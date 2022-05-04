cc5a43ecc9408277e119a4f2042ab4c1ee00c376
Davi Cardoso <davi.almeida.c@gmail.com>
2022-05-03 22:17:37 -0300

    component and props concept tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button (props: ButtonProps) {
+  return <button>{props.text ?? 'Default'}</button>
+}
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Button text="Enviar"/>
+      <Button text="Ok"/>
+      <Button />
+    </>
+  )
 }
 
 export default App
